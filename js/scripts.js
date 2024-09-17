1f9740b47067a4fe9faf8f59badc16abb8c7752d
bayuufa <akunbayu18@gmail.com>
2024-09-17 23:43:53 +0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,25 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+(function() {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // Hamburger Menu Toggle
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.getElementById('nav-menu');
+
+    menuToggle.addEventListener('click', () => {
+        document.body.classList.toggle('nav-active');
+    });
+})();
+
 // Hamburger Menu Toggle
 const menuToggle = document.getElementById('menu-toggle');
 const navMenu = document.getElementById('nav-menu');
